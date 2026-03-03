be7f1e28e665b623b8f4a104c47d79ea23c6a91f
Jakub Wolodko <wolodko.jakub@gmail.com>
2026-03-03 09:44:52 +0100

    Dodaj pliki projektów.

---
+++ TablicaOgloszen/Controllers/PostsController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TablicaOgloszen.Models;
+using System.Linq;
+
+namespace TablicaOgloszen.Controllers
+{
+    public class PostsController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public PostsController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Posts
+        public async Task<IActionResult> Index(int page = 1)
+        {
+            DateTime now = DateTime.Now;
+            DateTime sinseWhen = now.AddDays(-10);
+
+            var posts = _context.Posty
+                .Where(p => p.Date >= sinseWhen)
+                .OrderByDescending(p => p.Date);
+
+            int pageSize = 2;
+            var postsReturn = await posts
+                .Skip((page - 1) * pageSize)
+                .Take(pageSize)
+                .ToListAsync();
+
+            ViewBag.CurrentPage = page;
+
+            return View(postsReturn);
+        }
+
+
+        // GET: Posts/Create
+        public async Task<IActionResult> Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Title,Description")] Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(post);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(post);
+        }
+
+        // GET: Posts/Details/:id
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+            var post = await _context.Posty
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+
+        // GET: Posts/ConfirmPost
+        public async Task<IActionResult> ConfirmPost(Post post)
+        {
+            if (post == null)
+            {
+                return NotFound();
+            }
+            return View(post);
+        }
+    }
+}
---
+++ TablicaOgloszen/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data;
+
+namespace TablicaOgloszen.Models
+{
+    public class Post
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; } = DateTime.Now;
+    }
+}
