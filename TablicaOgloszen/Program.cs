c5d2e53eaa325dea69c2a507076de42537d095aa
Jakub Wolodko <wolodko.jakub@gmail.com>
2026-03-04 13:31:06 +0100

    Add logger

--- TablicaOgloszen/Program.cs
+++ TablicaOgloszen/Program.cs
@@ -1,4 +1,5 @@
 using Microsoft.EntityFrameworkCore;
+using Serilog;
 using TablicaOgloszen.Interfaces;
 using TablicaOgloszen.Services;
 
@@ -8,9 +9,22 @@ namespace TablicaOgloszen
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/all.log", rollingInterval: RollingInterval.Day)
+                .WriteTo.Logger(lc => lc
+                    .Filter.ByIncludingOnly(e =>
+                        e.Properties.ContainsKey("SourceContext") &&
+                        e.Properties["SourceContext"].ToString().Contains("TablicaOgloszen.Services"))
+                    .WriteTo.File("logs/dev.log", rollingInterval: RollingInterval.Day))
+                .CreateLogger();
+
+
             var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
+            builder.Host.UseSerilog();
+
             builder.Services.AddControllersWithViews();
             builder.Services.AddDbContext<AppDbContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
@@ -19,17 +33,14 @@ namespace TablicaOgloszen
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (!app.Environment.IsDevelopment())
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
             app.UseHttpsRedirection();
             app.UseRouting();
-
             app.UseAuthorization();
 
             app.MapStaticAssets();
--- TablicaOgloszen/Services/PostsService.cs
+++ TablicaOgloszen/Services/PostsService.cs
@@ -7,15 +7,17 @@ namespace TablicaOgloszen.Services
     public class PostsService : IPostsService
     {
         private readonly AppDbContext _context;
-
-        public PostsService(AppDbContext context)
+        private readonly ILogger<PostsService> _logger;
+        public PostsService(AppDbContext context, ILogger<PostsService> logger)
         {
             _context = context;
+            _logger = logger;
         }
 
         public async Task<List<Post>> GetRecentPosts(int days, int page, int pageSize)
         {
             DateTime sinceWhen = DateTime.Now.AddDays(-days);
+            _logger.LogInformation("Page {page} visited", page);
 
             return await _context.Posts
                 .Where(p => p.Date >= sinceWhen)
@@ -23,10 +25,12 @@ namespace TablicaOgloszen.Services
                 .Skip((page - 1) * pageSize)
                 .Take(pageSize)
                 .ToListAsync();
+
         }
 
         public async Task<Post?> GetPostById(int id)
         {
+            _logger.LogInformation("Post {id} recived", id);
             return await _context.Posts.FirstOrDefaultAsync(p => p.Id == id);
         }
 
@@ -34,6 +38,7 @@ namespace TablicaOgloszen.Services
         {
             _context.Posts.Add(post);
             await _context.SaveChangesAsync();
+            _logger.LogInformation("Post {name} has been created", post.Title);
         }
     }
 }
